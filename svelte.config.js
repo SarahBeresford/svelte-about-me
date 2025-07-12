59b97089062b64f90e91fbf8616cc8bc6858f6a0
SarahBeresford <sarah.beresford@stu.bcc.cuny.edu>
2025-07-12 19:17:49 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 export default {
+  build: {
+    outDir: 'docs'
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
