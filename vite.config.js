c8588ef84be86a4b36a83ead3c9a40d36d37e797
SarahBeresford <sarah.beresford@stu.bcc.cuny.edu>
2025-07-12 19:08:27 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import ghPages from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  plugins: [svelte(), ghPages()],
-  base: '/svelte-about-me/', // Replace with your repo name
-});
+  base: '/svelte-about-me',    // right here! your repository name
+  plugins: [svelte()],
+})
